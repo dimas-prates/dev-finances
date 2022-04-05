ffb8f2628aaa43f6ae2b3242250f5b2d75fc4b98
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-04-05 02:05:34 -0300

    Removed tags with data from HTML so the script.js file can add it. On script.js file has now the function to format the number and currency, it's beeing used DOM to return tags back to the html file. Used forEach and ternary condition also as well.

--- script.js
+++ script.js
@@ -10,3 +10,76 @@ const Modal = {
         document.querySelector('.modal-overlay').classList.remove('active')
     }
 }
+
+const transactions = [{
+    id: 1,
+    description: "Rent",
+    amount: -80000,
+    date: "2022-01-05"
+},
+{
+    id: 2,
+    description: "Internet",
+    amount: -20000,
+    date: "2022-01-10"
+},
+{
+    id: 3,
+    description: "Web page (job)",
+    amount: 500000,
+    date: "2022-01-23"
+},
+{
+    id: 4,
+    description: "Stocks",
+    amount: 20000,
+    date: "2022-01-30"
+}]
+
+const Transaction = {
+    income(){
+
+    },
+    outcome(){
+
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        DOM.transactionsContainer.appendChild(tr)
+
+    },
+    innerHTMLTransaction(transaction) {
+        const CCSClasses = transaction.amount < 0 ? "expense" : "income"
+        const amount = Utils.formatCurrency(transaction.amount);
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="${CCSClasses}">${amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Delete transaction">
+            </td>
+        `
+        return html
+    }
+}
+
+const Utils = {
+    formatCurrency(value){
+        const sign = Number(value) < 0 ? "&minus;" : ""
+        value = String(value).replace(/\D/g, "")
+        value = (Number(value)/100).toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+        return `${sign}${value}`;
+    }
+};
+
+transactions.forEach( function(transaction) {
+    DOM.addTransaction(transaction);
+});
